336b0edc2477c98edb5f7e6104d9284787725d0b
New Robot <73787667+new-robot@users.noreply.github.com>
2020-11-01 16:36:07 +0100

    Update program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace temp_repo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello guys !");
         }
     }
 }
