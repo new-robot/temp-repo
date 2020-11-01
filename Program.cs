74521f1029bd97c45cdbfc403f4a020eeb5bce09
New Robot <73787667+new-robot@users.noreply.github.com>
2020-11-01 17:55:52 +0100

    simplify program

--- Program.cs
+++ Program.cs
@@ -6,7 +6,47 @@ namespace temp_repo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello guys !");
+            Console.WriteLine("Bonjour");
+
+            AskQuestion("veuillez entrer votre pseudo :", "Voici votre pseudo: ");
+            AskQuestion("veuillez entrer votre age :", "Voici votre age: ");
+            DoYouLikeCats();
+
+            // Fin
+            Console.WriteLine("Appuyez sur un touche pour mettre fin au programme...");
+            Console.ReadLine();
+        }
+
+        static void AskQuestion(string question, string reponse)
+        {
+            Console.WriteLine(question);
+            string texte = Console.ReadLine();
+            Console.WriteLine(reponse + texte);
+            
+        }
+
+        static void DoYouLikeCats()
+        {            
+            bool goodAnswer = false;
+            do
+            {
+                Console.WriteLine("Aimez-vous les chats ? (o/n)");
+                string reponseChats = Console.ReadLine();
+                if (reponseChats == "o")
+                {
+                    goodAnswer = true;
+                    Console.WriteLine("D'accord, vous aimez les chats.");
+                }
+                else if (reponseChats == "n")
+                {
+                    goodAnswer = true;
+                    Console.WriteLine("D'accord vous n'aimez pas les chats.");
+                } 
+                else
+                {
+                    Console.WriteLine("Veuillez choisir entre (o/n)");
+                }
+            } while (!goodAnswer);
         }
     }
 }
