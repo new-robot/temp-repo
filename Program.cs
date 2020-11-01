d41b8fefd55a05f8ca8a74b827e9cd5e4b53762c
ludwigdn <11355187+ludwigdn@users.noreply.github.com>
2020-11-01 16:20:57 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace temp_repo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
